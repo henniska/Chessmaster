6de0803a27f4202ed6beaa3b864e74e34d015179
Henri Niskanen <henri.niskanen@helsinki.fi>
2015-06-11 23:59:00 +0300

    Testejä

--- shakkiohjelma/src/main/java/shakki/shakkiohjelma/ohjelmalogiikka/NewMain.java
+++ shakkiohjelma/src/main/java/shakki/shakkiohjelma/ohjelmalogiikka/NewMain.java
@@ -1,9 +1,22 @@
 
 package shakki.shakkiohjelma.ohjelmalogiikka;
 
+import java.util.Arrays;
+
 public class NewMain {
 
     public static void main(String[] args) {
+        Logiikka logiikka = new Logiikka();
+        String testiLauta[][] = {
+        {"r", "k", "b", "q", "c", "b", "k", "r"},
+        {"p", "p", "p", "p", "p", "p", "p", "p"},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {"P", "P", "P", "P", "P", "P", "P", "P"},
+        {"R", "K", "B", "Q", "C", "B", "K", "R"}};
+        logiikka.vaihdaLauta(testiLauta);
         System.out.println("Siirrot:");
         System.out.println(Logiikka.mahdollisetSiirrot());
 
--- shakkiohjelma/src/test/java/LogiikkaTest.java
+++ shakkiohjelma/src/test/java/LogiikkaTest.java
@@ -9,7 +9,8 @@ import shakki.shakkiohjelma.ohjelmalogiikka.Logiikka;
 
 
 public class LogiikkaTest {
-    String siirrot = "";
+    Logiikka logiikka = new Logiikka();
+            
     
     public LogiikkaTest() {
     }
@@ -24,7 +25,6 @@ public class LogiikkaTest {
     
     @Before
     public void setUp() {
-        siirrot = siirrot + Logiikka.mahdollisetSiirrot();
     }
     
     @After
@@ -38,28 +38,85 @@ public class LogiikkaTest {
     // public void hello() {}
     
     @Test
-    public void kuningasLiikkeetAloitusTilanteessa() {
-        String kuningasLiikkeet = Logiikka.mahdollisetC(60);
-        assertEquals("", kuningasLiikkeet);
+    public void liikkeetAloitusTilanteessa() {
+        String testiLauta[][] = {
+        {"r", "k", "b", "q", "c", "b", "k", "r"},
+        {"p", "p", "p", "p", "p", "p", "p", "p"},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {"P", "P", "P", "P", "P", "P", "P", "P"},
+        {"R", "K", "B", "Q", "C", "B", "K", "R"}};
+        logiikka.vaihdaLauta(testiLauta);
+        String liikkeet = logiikka.mahdollisetSiirrot();
+        assertEquals("6050 6040 6151 6141 6252 6242 6353 6343 6454 6444 6555 6545 6656 6646 6757 6747 7150 7152 7655 7657 ", liikkeet);
     }
     
     @Test
     public void kuningasLiikkeetTyhjassaTilassa() {
-        String kuningasLiikkeet = Logiikka.mahdollisetC(35);
+        String testiLauta[][] = {
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", "C", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "}};
+        logiikka.vaihdaLauta(testiLauta);
+        String kuningasLiikkeet = logiikka.mahdollisetC(35);
         assertEquals("4332 4333 4334 4342 4344 4352 4353 4354 ", kuningasLiikkeet);
     }
     
     @Test
     public void kuningasLiikkeetLaudanLaidassa() {
-        String kuningasLiikkeet = Logiikka.mahdollisetC(32);
+        String testiLauta[][] = {
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {"C", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "}};
+        logiikka.vaihdaLauta(testiLauta);
+        String kuningasLiikkeet = logiikka.mahdollisetSiirrot();
         assertEquals("4030 4031 4041 4050 4051 ", kuningasLiikkeet);
     }
     
     @Test
     public void kuningasNappuloidenSyominen() {
-        String kuningasLiikkeet = Logiikka.mahdollisetC(16);
-        assertEquals("2010p2011p2021 2030 2031 ", kuningasLiikkeet);
+        String testiLauta[][] = {
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {"p", "p", " ", " ", " ", " ", " ", " "},
+        {"C", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "}};
+        logiikka.vaihdaLauta(testiLauta);
+        String kuningasLiikkeet = logiikka.mahdollisetC(16);
+        assertEquals("2010p2011p2030 2031 ", kuningasLiikkeet);
     }
+    
+    @Test
+    public void onkoKuningasTurvassa() {
+        String testiLauta[][] = {
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", "b", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", "C", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "}};
+        logiikka.vaihdaLauta(testiLauta);
+        boolean vastaus = logiikka.onkoKuningasTurvassa();
+        assertEquals(false, vastaus);
+    }
+    
     
     /* Lisään myöhemmin enemmän testejä.
      */
